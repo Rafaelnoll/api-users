600914d4842340699208897dd1c69bc08869099f
Rafael Noll <rafanoll2004@gmail.com>
2022-08-16 15:01:38 -0300

    conexão com o banco de dados(mongoDB) + váriaveis de ambiente

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
+require("dotenv/config");
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+mongoose.connect(process.env.DATABASE_URI);
 
 app.get("/", (req, res) => {
     res.json({});
